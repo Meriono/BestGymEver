e851ca31a6efe07774108a4ae2d6bfd8ae43a7d9
Hanna Edlund <hanna@edlunds.biz>
2020-10-08 13:50:22 +0200

    Added test

---
+++ test/Inlämning/Inlämning2/BestGymEverTest.java
@@ -0,0 +1,13 @@
+package Inlämning.Inlämning2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Hanna Edlund
+ * Date: 2020-10-08
+ * Time: 13:47
+ * Project: Sprint2
+ */
+class BestGymEverTest {
+
+}
