56c16663e27adaa3328f3702ad7a80b51ddaeffb
Hanna Edlund <hanna@edlunds.biz>
2020-10-08 15:26:21 +0200

    Lagt till getInput metod Påbörjat metod, printMember som skriver ut om medlemmen finns i registret eller inte Lagt till metod som läser in en fil och gör om denna till en lista

--- src/Inlämning/Inlämning2/BestGymEver.java
+++ src/Inlämning/Inlämning2/BestGymEver.java
@@ -1,5 +1,10 @@
 package Inlämning.Inlämning2;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  * Created by Hanna Edlund
  * Date: 2020-10-08
@@ -7,6 +12,54 @@ package Inlämning.Inlämning2;
  * Project: Sprint2
  */
 public class BestGymEver {
+
+    String members = "customers.txt";
+
+    public List<String> getListFromFile(String members){
+        List<String> fullList = new ArrayList<>();
+        try {
+            Scanner sc = new Scanner(new File(members));
+            while(sc.hasNextLine()){
+                fullList.add(sc.nextLine());
+            }
+        }
+        catch (Exception e){
+            System.out.println("Error");
+            System.exit(0);
+        }
+        return fullList;
+    }
+
+    public void printMember(List<String> fullList, String input){
+        for (String s: fullList) {
+            if(s.contains(input))
+                System.out.println("MEDLEM");
+        }
+    }
+
+    public String getInput(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Skriv in en medlems namn eller personnummer");
+        String input = null;
+
+        while (sc.hasNextLine()){
+            input = sc.nextLine();
+            if(input.equals("KLAR"))
+                break;
+
+            return input;
+        }
+        return input;
+    }
+
+    public BestGymEver(){
+        List<String> fullList = getListFromFile(members);
+        printMember(fullList, getInput());
+    }
+
+    public static void main(String[] args) {
+        BestGymEver bge = new BestGymEver();
+    }
 }
 
 /*
