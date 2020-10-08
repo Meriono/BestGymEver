f733bb96f4a274a94faee449c6957b8756bf7566
Hanna Edlund <hanna@edlunds.biz>
2020-10-08 13:45:54 +0200

    Initial commit

---
+++ src/Inlämning/Inlämning2/BestGymEver.java
@@ -0,0 +1,24 @@
+package Inlämning.Inlämning2;
+
+/**
+ * Created by Hanna Edlund
+ * Date: 2020-10-08
+ * Time: 13:36
+ * Project: Sprint2
+ */
+public class BestGymEver {
+}
+
+/*
+        Ta input, namn eller personnummer
+        läs i filen om personen finns.
+            finns? När betalades årsavgiften.
+                inom ett år - MEDLEM
+                för över ett år sen - FÖREDETTA MEDLEM
+            finns inte? - INTE MEDLEM
+
+
+            Om MEDLEM
+            skriv ner när kund besökt gymmet i en egen fil.
+
+ */
